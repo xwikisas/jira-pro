b7138964e78bc155ca4813dc24bb52193a8dc741
Josué Tille <josue.tille@xwiki.com>
2025-07-21 16:37:58 +0200

    #1: Add support for authentication with OAuth in JIRA

---
+++ jira-pro-oauth/jira-pro-oauth-api/src/main/java/com/xwiki/jirapro/oauth/internal/JIRAOAuthAuthenticator.java
@@ -0,0 +1,149 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xwiki.jirapro.oauth.internal;
+
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.hc.client5.http.ContextBuilder;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.core5.http.HttpHost;
+import org.slf4j.Logger;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.InstantiationStrategy;
+import org.xwiki.component.descriptor.ComponentInstantiationStrategy;
+import org.xwiki.contrib.jira.config.JIRAAuthenticator;
+import org.xwiki.contrib.jira.config.JIRAServer;
+import org.xwiki.contrib.oidc.OAuth2ClientManager;
+import org.xwiki.contrib.oidc.OAuth2Token;
+import org.xwiki.contrib.oidc.OAuth2TokenStore;
+import org.xwiki.contrib.oidc.auth.store.OIDCClientConfiguration;
+import org.xwiki.contrib.oidc.auth.store.OIDCClientConfigurationStore;
+import org.xwiki.job.Job;
+
+import com.atlassian.jira.rest.client.api.AuthenticationHandler;
+
+/**
+ * JIRA Rest Client provider with OAuth configuration.
+ *
+ * @version $Id$
+ * @since 1.0.0
+ */
+@Component
+@InstantiationStrategy(ComponentInstantiationStrategy.PER_LOOKUP)
+@Named(JIRAOAuthAuthenticator.HINT)
+public class JIRAOAuthAuthenticator implements JIRAAuthenticator
+{
+    /**
+     * The authenticator HINT.
+     */
+    public static final String HINT = "oauth";
+
+    private static final String ONLY_OAUTH_JIRA_SERVER_SUPPORTED =
+        "DefaultRestClientProvider only support OAuthJIRAServer class";
+
+    private String configurationName;
+
+    private boolean isRequiringAuthentication;
+
+    @Inject
+    private OIDCClientConfigurationStore clientConfigurationStore;
+
+    @Inject
+    private OAuth2ClientManager oAuth2ClientManager;
+
+    @Inject
+    private OAuth2TokenStore tokenStore;
+
+    @Inject
+    private Logger logger;
+
+    public void configure(String configurationName, boolean isRequiringAuthentication)
+    {
+        this.configurationName = configurationName;
+        this.isRequiringAuthentication = isRequiringAuthentication;
+    }
+
+    @Override
+    public AuthenticationHandler getRestClientAuthenticationHandler()
+    {
+        throw new RuntimeException("Feature not supported to configure a AuthenticationHandler for OAuth");
+    }
+
+    @Override
+    public void authenticateInHttpClient(ContextBuilder context, HttpUriRequest request,
+        HttpHost targetHost)
+    {
+        Optional<String> token = getOAuthToken();
+        token.ifPresent(s -> request.setHeader("Authorization", "Bearer " + s));
+    }
+
+    @Override
+    public boolean isAuthenticatingRequest()
+    {
+        return getOAuthToken().isPresent();
+    }
+
+    @Override
+    public String getId()
+    {
+        return configurationName;
+    }
+
+    public Optional<String> getOAuthToken()
+    {
+        try {
+            // Make sure that the token is up to date
+            OIDCClientConfiguration configuration =
+                clientConfigurationStore.getOIDCClientConfiguration(configurationName);
+            Job tokenRenewalJob = oAuth2ClientManager.renew(configuration);
+            if (tokenRenewalJob != null) {
+                try {
+                    tokenRenewalJob.join();
+                } catch (InterruptedException e) {
+                    logger.error("Can't get renewal job", e);
+                    return Optional.empty();
+                }
+            }
+
+            OAuth2Token token = tokenStore.getToken(configuration);
+            if (token != null) {
+                return Optional.of(token.getAccessToken());
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            logger.error("Can't OAuth token", e);
+            return Optional.empty();
+        }
+    }
+
+    public String getConfigurationName()
+    {
+        return configurationName;
+    }
+
+    public boolean isRequiringAuthentication()
+    {
+        return isRequiringAuthentication;
+    }
+}
---
+++ jira-pro-oauth/jira-pro-oauth-api/src/main/java/com/xwiki/jirapro/oauth/internal/JIRAOAuthAuthenticatorFactory.java
@@ -0,0 +1,95 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xwiki.jirapro.oauth.internal;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+import org.apache.commons.lang3.StringUtils;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.component.manager.ComponentManager;
+import org.xwiki.contrib.jira.config.JIRAAuthenticator;
+import org.xwiki.contrib.jira.config.JIRAuthenticatorFactory;
+import org.xwiki.model.reference.LocalDocumentReference;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
+@Singleton
+@Component
+@Named(JIRAOAuthAuthenticator.HINT)
+public class JIRAOAuthAuthenticatorFactory implements JIRAuthenticatorFactory
+{
+    private static final List<String> JIRA_PRO_OAUTH_SPACE = List.of("XWiki", "JIRAPro", "OAuth");
+
+    private static final String CONFIG_ID_FIELD = "id";
+
+    @Inject
+    @Named("context")
+    private Provider<ComponentManager> componentManagerProvider;
+
+    /**
+     * The document for storing the basic auth configuration.
+     */
+    public static final LocalDocumentReference OAUTH_CONFIG_REFERENCE =
+        new LocalDocumentReference(JIRA_PRO_OAUTH_SPACE, "Config");
+
+    /**
+     * The class reference for the basic auth configuration.
+     */
+    public static final LocalDocumentReference OAUTH_DATA_CLASS_REFERENCE =
+        new LocalDocumentReference(JIRA_PRO_OAUTH_SPACE, "ConfigClass");
+
+    @Inject
+    private Provider<XWikiContext> contextProvider;
+
+    @Override
+    public JIRAAuthenticator get(String serverId)
+    {
+        XWikiContext context = contextProvider.get();
+        try {
+            XWikiDocument doc = context.getWiki()
+                .getDocument(OAUTH_CONFIG_REFERENCE, context);
+            Optional<BaseObject> authObj = doc.getXObjects(OAUTH_DATA_CLASS_REFERENCE)
+                .stream().filter(x -> StringUtils.equals(serverId, (x.getStringValue(CONFIG_ID_FIELD))))
+                .findFirst();
+            if (authObj.isPresent()) {
+                int requireAuthentication = authObj.get().getIntValue("requireAuthentication");
+                String configName = authObj.get().getStringValue("oidcConfigName");
+                JIRAOAuthAuthenticator authenticator =
+                    componentManagerProvider.get().getInstance(JIRAAuthenticator.class, JIRAOAuthAuthenticator.HINT);
+                authenticator.configure(configName, requireAuthentication == 1);
+                return authenticator;
+            } else {
+                throw new RuntimeException("Can't find Basic auth config for server ID: " + serverId);
+            }
+        } catch (XWikiException | ComponentLookupException e) {
+            throw new RuntimeException("Can't get JIRA Basic auth configuration", e);
+        }
+    }
+}
